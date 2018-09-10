134c4950b07b173a2d58b40d8acc1e8ad3c0e101
Your Name <hokandre@mhs.mdp.ac.id>
2018-09-10 23:03:21 +0700

    calculator

--- calculator.js
+++ calculator.js
@@ -3,20 +3,52 @@
 class Calculator {
   //write your code here
   constructor () {
+    this.angkaAwal=0
   }
-  add () {
+  add (angka) {
+    this.angkaAwal+=angka
+    return this
   }
-  subtract () {
+  subtract (angka) {
+    this.angkaAwal -= angka
+    return this
   }
-  multiply () {
+  multiply (angka) {
+    if(this.angkaAwal===0){
+      this.angkaAwal=angka*1
+    } 
+    else{
+      this.angkaAwal= angka*this.angkaAwal
+    }
+    return this
   }
-  divide () {
+  divide (angka) {
+    if(this.angkaAwal===0){
+      this.angkaAwal=this.angkaAwal/1
+    } 
+    else{
+      this.angkaAwal=this.angkaAwal/angka
+    }
+    return this
   }
-  square () {
+  square (pangkat) {
+    let result=Math.pow(this.angkaAwal,pangkat)
+    this.angkaAwal=result
+    return this
   }
   squareRoot () {
+    let result=Math.sqrt(this.angkaAwal)
+    this.angkaAwal=result
+    return this
+  }
+  circle(jari){
+    this.angkaAwal=Math.PI*Math.pow(jari,2)
+    return this;
   }
 }
+let soal =new Calculator;
+console.log(soal.circle(7))
+
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
