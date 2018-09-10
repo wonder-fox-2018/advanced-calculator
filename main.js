38264819208ca2ed0121a276e8c327899ad31745
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-09-10 22:20:22 +0700

    push

--- main.js
+++ main.js
@@ -3,3 +3,7 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+
+const hitung = new Calculator(1).add(3).subtract(2).multiply(3).lingkaran(4)
+
+console.log(hitung)
