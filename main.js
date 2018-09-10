e838e667a22394a83be33addedb9677e320f4c3c
hadinw <hadiykk99@gmail.com>
2018-09-10 11:12:55 +0700

    calc first commit

--- main.js
+++ main.js
@@ -3,3 +3,5 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+let calc = new Calculator(10)
+console.log(calc.add(5).subtract(1).multiply(2).divide(2).square(2).squareRoot().num)
