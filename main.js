d91e7ccf1ea8e4a6fcc76535b34736724de1c96a
agungatidhira@Nyawaku <agungatidhira@gmail.com>
2018-09-10 11:22:30 +0700

    finish all features calculator

--- main.js
+++ main.js
@@ -3,3 +3,11 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+const calculator = new Calculator(1)
+
+console.log(calculator.add(2).subtract(1).multiply(11).divide(2).square().squareRoot().num)
+
+const calculator2 = new Calculator(3)
+console.log(calculator2.toThePower(3).num)
+calculator2.subtract(20)
+console.log(calculator2.circleArea().num)
