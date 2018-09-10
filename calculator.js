1a387e19a9f62d1a6d5057aa625c103fbf95eb98
reshalem <aliisetia@gmail.com>
2018-09-10 22:01:25 +0700

    Tugas Calculator

--- calculator.js
+++ calculator.js
@@ -1,21 +1,43 @@
 'use strict'
 
 class Calculator {
-  //write your code here
-  constructor () {
-  }
-  add () {
-  }
-  subtract () {
-  }
-  multiply () {
-  }
-  divide () {
-  }
-  square () {
-  }
-  squareRoot () {
-  }
+    constructor (initialNumber) {
+        this.initialNumber = initialNumber;
+    }
+
+    add (addedBy) {
+        this.initialNumber = this.initialNumber + addedBy;
+        return this;
+    }
+
+    subtract (subtractedBy) {
+        this.initialNumber = this.initialNumber - subtractedBy;
+        return this;
+    }
+
+    multiply (multipliedBy) {
+        this.initialNumber = this.initialNumber * multipliedBy;
+        return this;
+    }
+
+    divide (dividedBy) {
+        this.initialNumber = this.initialNumber / dividedBy;
+        return this;
+    }
+
+    square (squaredBy) {
+        var initialNumber = this.initialNumber;
+        for (var i = 0; i < squaredBy-1; i++) {
+            this.initialNumber = this.initialNumber * initialNumber;
+        }
+
+        return this;
+    }
+
+    squareRoot () {
+        this.initialNumber = Math.sqrt(this.initialNumber);
+        return this;
+    }
 }
 
 /** note : you can use several features from ecmascript, such as:
@@ -26,4 +48,8 @@ class Calculator {
 * - Method Chaining
 */
 
-module.exports = Calculator
+var calculator = new Calculator(36);
+
+console.log(calculator.squareRoot().add(10).add(200).subtract(150).divide(3).multiply(2));
+
+module.exports = Calculator;
