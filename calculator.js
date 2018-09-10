92d3ff80d512dfeccb885944d35b837f82b75f46
ajinfadhila <ajinfadhila@gmail.com>
2018-09-10 23:34:38 +0700

    up

--- calculator.js
+++ calculator.js
@@ -1,22 +1,57 @@
-'use strict'
+
+`use strict`
 
 class Calculator {
-  //write your code here
-  constructor () {
+  constructor(tetap) {
+      this.num = tetap
   }
-  add () {
+
+  tambah(num1){
+    this.num += num1
+    return this
   }
-  subtract () {
+  kurang(num1){
+
+    this.num -= num1
+    return this
   }
-  multiply () {
+  kali(num1){
+    this.num *= num1
+    return this
   }
-  divide () {
+  bagi(num1){
+    this.num /= num1
+    return this
   }
-  square () {
+  pangkat(num1) {
+    // console.log(num1)
+
+    for (var i = 0; i < num1-1; i++) {
+      this.num *= this.num
+    }
+    return this
+
   }
-  squareRoot () {
+
+  lingkaran(){
+    // this.num = Math.PI * num1 * num2
+    this.num *= Math.PI*2
+    return this;
+  }
+  squareRoot (num1) {
+    this.num *= Math.sqrt(num1)
   }
 }
+let calct = new Calculator()
+// console.log(calct.add(2,3))
+// console.log(calct.subtract(3,1))
+// console.log(calct.multiply(2,3))
+// console.log(calct.divide(6,2))
+// console.log(calct.square(3,3))
+// console.log(calct.squareRoot(3,3))
+// console.log(calct.circleArea(3,3))
+module.exports = Calculator
+
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
