7f7b4b376d3a01f39794756e91def442a617573b
pputra <mynameisyoga@live.com>
2018-09-10 22:48:30 +0700

    add solutiion

--- main.js
+++ main.js
@@ -2,4 +2,12 @@
 
 const Calculator = require("./calculator.js")
 
-// execute function on calculator.js in here
+var calculator = new Calculator();
+
+calculator.value = 10;
+calculator.add(5).square(2).squareRoot().divide(5).multiply(10).subtract(1).add(calculator.pi);
+
+console.log(calculator.value);
+
+calculator.reset();
+console.log(calculator.value);
